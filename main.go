4f14085e8b72c90028e610ddefd23dbaed5f35eb
Robin Hagmayer <robin.hagmayer@proton.me>
2025-08-03 17:40:34 +0200

    Added the readiness endpoint /healthz

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		r.Header["Content-Type"] = []string{"text/plain", "charset=utf-8"}
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 
 	server := &http.Server{
 		Addr:    ":" + port,
