e8b7ff5c8ec5fdf7ce29fe25345390163e62240d
Robin Hagmayer <robin.hagmayer@proton.me>
2025-08-03 14:35:05 +0200

    Added a simple server

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello Chirpy!")
+	const port = "8080"
+
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
 }
